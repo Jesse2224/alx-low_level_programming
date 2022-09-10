ea1448dc9d8c2e1b033b4da48321c3a7042a6247
Anthony <godson4life2224@gmail>
2022-09-09 17:07:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+char ch, e, q;
+
+	e = 'e';
+	q = 'q';
+
+for (ch = 'a'; ch <= 'z'; low++)
+{
+if (ch != e && ch != q)
+{
+	putchar(ch);
+}
+}
+printf("\n");
+return (0);
+}
