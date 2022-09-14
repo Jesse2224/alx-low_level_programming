3949f51ae25d2cffcd79233758ecdaa59a085f2e
Anthony <godson4life2224@gmail>
2022-09-14 04:06:34 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,20 @@
 */
 void print_times_table(int n)
 {
+	print_times_table(15);
 	return (0);
+}
 
+int print_times_table(int n){
+	if(n<=15 && n>=0){
+		for (int col=0; col<=n; col++){
+			for  (int row=0; row<=n; row++) {
+			print("% \t", col*row);
+			if(row==n){
+				printf("\n");
+			}
+		}
+	}
+}
+return 0;
 }
