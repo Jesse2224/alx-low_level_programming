90dccbda38e10d100d66d48d79295d882e7948cc
Anthony <godson4life2224@gmail>
2022-09-15 16:54:13 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,19 +13,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 	putchar('-');
-	n = -n;
-	}
-
-	if (n == 0)
-	putchar('0');
-
-	if (n / 10)
-	printnumber(n / 10);
-	putchar(n % 10 + '0');
+	num = -num;
 }
+	if (num > 0)
 {
-	int n = atoi(argv[1]);
-
-	printnumber(n);
-	printf("\n");
+	print_number(num / 10);
+}
+	putchar(num % 10 + '0');
 }
